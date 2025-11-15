f6801c9b9070523122f19ce883987df3474c1687
img1982-nano <ddoossyyu+img1982@gmail.com>
2025-11-15 23:16:19 +0900

    Add: src/routes/[slug]/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
     import { pageOpen } from "$lib/pageOpen";
+    import { Card, Button, Toggle } from "flowbite-svelte";
+    import { ArrowRightOutline } from "flowbite-svelte-icons";
+
+    let vCard = false;
 </script>
 
-<div class="card bg-base-100 w-96 shadow-sm">
-    <figure>
-        <img
-            src="https://www.secom-sanin.co.jp/images/original/20180725115339927bd176c24.jpeg"
-            alt="Shoes"
-        />
-    </figure>
-    <div class="card-body">
-        <h2 class="card-title">小学１年生算数</h2>
-        <p>基礎中の基礎です。</p>
-        <div class="card-actions justify-end">
-            <button class="btn btn-primary" onclick={() => pageOpen(1)}
-                >学習を開始する</button
+<div class="space-y-4">
+    <Card
+        img="https://www.secom-sanin.co.jp/images/original/20180725115339927bd176c24.jpeg"
+        reverse={vCard}
+    >
+        <div class="m-6">
+            <h5
+                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
             >
+                小学１年生算数
+            </h5>
+            <p
+                class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400"
+            >
+                基礎中の基礎です
+            </p>
+            <Button class="w-40" onclick={() => pageOpen(1)}>
+                学習を開始 <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
+            </Button>
         </div>
-    </div>
+    </Card>
 </div>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { ai_question } from "$lib/ai_question.js";
     import { pageOpen } from "$lib/pageOpen";
+    import { Button, Modal, P } from "flowbite-svelte";
 
     let { data } = $props();
     let explain = $state();
@@ -9,6 +10,7 @@
     let ready_check = $state(false);
     let user_input = $state();
     let check_toggle;
+    let defaultModal = $state(false);
     const next = Number(data.slug) + 1;
     const back = Number(data.slug) - 1;
 
@@ -26,11 +28,15 @@
             );
             if (user_input === data.Mondai.answer) {
                 check_input = true;
+                confetti({
+                    particleCount: 100,
+                });
             } else {
                 check_input = false;
             }
             console.log(explain, check_input);
             do_ai_gen = false;
+            defaultModal = true;
             return { explain, check_input };
         }
         console.log(explain, check_input);
@@ -41,24 +47,26 @@
 
 <h1>問題:{data.Mondai.problem}</h1>
 <input placeholder="回答を入力" bind:value={user_input} />
-<button onclick={() => check()}>採点</button>
+<Button onclick={() => check()}>採点</Button>
 {#if ready_check === true}
     {#if do_ai_gen === true}
         <p>採点中です</p>
     {/if}
-    {#if check_input === true}
-        <h1>正解だと思う</h1>
-        <p>解説:{explain}</p>
-        <button onclick={() => pageOpen(next)}>次の問題へ</button>
-        {#if Number(data.slug) > 1}
-            <button onclick={() => pageOpen(back)}>前の問題へ</button>
+    <Modal
+        title="採点結果"
+        form
+        bind:open={defaultModal}
+        onaction={({ action }) => pageOpen(next)}
+    >
+        {#if check_input === true}
+            <P>正解だと思う</P>
+            <P>解説:{explain}</P>
+        {:else if check_input === false}
+            <P>不正解だと思う</P>
+            <P>解説:{explain}</P>
         {/if}
-    {:else if check_input === false}
-        <h1>不正解だと思う</h1>
-        <p>解説:{explain}</p>
-        <button onclick={() => pageOpen(next)}>次の問題へ</button>
-        {#if Number(data.slug) > 1}
-            <button onclick={() => pageOpen(back)}>前の問題へ</button>
-        {/if}
-    {/if}
+        {#snippet footer()}
+            <Button type="submit" value="success">次の問題へ</Button>
+        {/snippet}
+    </Modal>
 {/if}
