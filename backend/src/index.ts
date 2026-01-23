8566b3fab48f9e2360778d94f5241c8fffc12e58
img1982-nano <ddoossyyu+img1982@gmail.com>
2026-01-23 19:46:00 +0900

    Return index.ts: Get DB for MondaiColleciton

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,20 @@
+//Library
 import { Elysia, t } from "elysia";
 import mongo from "mongoose";
 import { createWorker } from "tesseract.js";
 import { queryCoercions } from "elysia/dist/replace-schema";
 import { Mondai, test_collection } from "./config";
-
+//variable
 const worker = await createWorker("eng+jpn")
 const app = new Elysia()
 const mongodb_password = process.env.MONGODB_PASSWORD
+//function
 async function startup() {
   try {
+    // MongoDBに接続
     await mongo.connect(`mongodb+srv://azilamo:${mongodb_password}@cluster0.dyt4o.mongodb.net/test?appName=Cluster0`)
     console.log("[BACKEND] connect to database compleat")
+    // APiサーバーを起動
     app.listen(3000)
     console.log(
       `[BACKEND] startup to http://${app.server?.hostname}:${app.server?.port}`
@@ -21,14 +25,17 @@ async function startup() {
   }
 }
 async function image_ocr(image: Blob) {
+  // Tesseractを使用したOCRの関数
   const image_data = await image.arrayBuffer()
   const { data: { text } } = await worker.recognize(Buffer.from(image_data))
   const response = text
   await worker.terminate()
+  // レスポンスから空白を削除して返す
   return text.replace(/\s/g, "")
 }
-app.get("/", () => "バックエンドAPIサーバー")
-/*app.get("/api/test", async () => {
+//api
+app.get("/", () => "FuckYou")
+app.get("/api/db/Mondai", async () => {
   const response = await Mondai.find()
   console.log(`User connect to /aieu, response to :${response}`)
   return response
@@ -36,17 +43,21 @@ app.get("/", () => "バックエンドAPIサーバー")
   query: t.Object({
     collection: t.String()
   })
-  })*/
+})
 app.post("/api/ocr", async ({ body }) => {
+  // OCRの関数を呼び出す
   const response = await image_ocr(body.file)
   console.log(response)
   return response
 }, {
+  //Schema
   body: t.Object({
     file: t.File({ format: "image/*" })
   })
 })
 app.get("/api/test_2", async ({ query }) => {
+  //MongoDbから特定のコレクションのデータをすべて取得
+  //{url}/api/test_2?collection={データベースのコレクション名}
   const response = await mongo.connection.collection(query.collection).find({}).toArray()
   console.log(`User connect to test_2 api, response to :${response}`)
   return response
@@ -60,4 +71,6 @@ app.get("/api/test_2", async ({ query }) => {
 //    mongo.
 //  }
 //)
+
+//startup
 startup()
